7c86051066e44a34b1463101d3a149ed7f8ae00c
AdhithyaRS <toadhi1509@gmail.com>
2023-09-14 13:13:26 +0530

    Update Output.java

--- MergeHeapQuick/src/main/java/sorting/Output.java
+++ MergeHeapQuick/src/main/java/sorting/Output.java
@@ -7,5 +7,10 @@ public class Output {
 		 * Merge Sort Time: 109ms
 		 * Quick Sort Time: 67ms
 		 * Heap Sort Time: 113ms
+		 * 
 		 */
+	// Note: Memory usage is difficult to measure directly in Java,
+    // but based on the algorithm characteristics, you can infer the following:
+    // Merge Sort uses O(n) extra space, Quick Sort uses O(log n) with an in-place sort,
+    // Heap Sort uses O(1) extra space.
 }
